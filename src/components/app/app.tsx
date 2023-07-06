467f58c06366f900b07f6e06141caa85e4fa248b
frymmox <frymmox@gmail.com>
2023-07-06 16:24:13 +0300

    Добавляет главный экран

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppProps = {
+  offers: number;
+}
+
+function App({offers}: AppProps): JSX.Element {
+  return (
+    <MainScreen offers={offers} />
+  );
+}
+
+export default App;
